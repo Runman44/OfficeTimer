9eb6ee9ee76cca5062667dd22dc14c9bef6c2d46
Dennis Anderson <info@mranderson.nl>
2016-08-09 08:47:32 +0200

    adjusted the UI that Tone will be a new list and set the text in the list itself

--- app/src/main/java/nl/mranderson/sittingapp/fragment/SettingsFragment.java
+++ app/src/main/java/nl/mranderson/sittingapp/fragment/SettingsFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,15 +13,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import nl.mranderson.sittingapp.R;
 import nl.mranderson.sittingapp.UserPreference;
 import nl.mranderson.sittingapp.Utils;
 
-public class SettingsFragment extends android.support.v4.app.Fragment implements CompoundButton.OnCheckedChangeListener {
+public class SettingsFragment extends android.support.v4.app.Fragment implements CompoundButton.OnCheckedChangeListener, View.OnClickListener {
 
     private CheckBox cSensors;
     private String tone;
+    private TextView cTone;
 
     public SettingsFragment() {
     }
@@ -39,12 +42,12 @@ public class SettingsFragment extends android.support.v4.app.Fragment implements
         CheckBox cVibration = (CheckBox) getActivity().findViewById(R.id.notify_vibration);
         CheckBox cLight = (CheckBox) getActivity().findViewById(R.id.notify_light);
         CheckBox cSound = (CheckBox) getActivity().findViewById(R.id.notify_sound);
-        CheckBox cTone = (CheckBox) getActivity().findViewById(R.id.notify_tone);
+        cTone = (TextView) getActivity().findViewById(R.id.notify_tone);
         cSensors.setOnCheckedChangeListener(this);
         cLight.setOnCheckedChangeListener(this);
         cSound.setOnCheckedChangeListener(this);
         cVibration.setOnCheckedChangeListener(this);
-        cTone.setOnCheckedChangeListener(this);
+        cTone.setOnClickListener(this);
 
         SharedPreferences prefs = getActivity().getSharedPreferences(UserPreference.MY_PREFS_NAME, getActivity().MODE_PRIVATE);
         Boolean light = prefs.getBoolean("light", true);
@@ -53,10 +56,18 @@ public class SettingsFragment extends android.support.v4.app.Fragment implements
         Boolean sensors = prefs.getBoolean("sensor", false);
         tone = prefs.getString("music", RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION).toString());
 
+        setRingToneText(tone);
+
         cLight.setChecked(light);
         cSound.setChecked(sound);
         cVibration.setChecked(vibration);
         cSensors.setChecked(sensors);
+    }
+
+    private void setRingToneText(String tone) {
+        Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), Uri.parse(tone));
+        String title = ringtone.getTitle(getActivity());
+        cTone.setText(title);
     }
 
 
@@ -83,16 +94,8 @@ public class SettingsFragment extends android.support.v4.app.Fragment implements
                     UserPreference.setSensorSettings(getActivity(), false);
                 }
                 break;
-            case R.id.notify_music:
-                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION);
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, Uri.parse(tone));
-                this.startActivityForResult(intent, 5);
-                break;
         }
     }
-
 
     @Override
     public void onActivityResult(final int requestCode, final int resultCode, final Intent intent) {
@@ -102,10 +105,27 @@ public class SettingsFragment extends android.support.v4.app.Fragment implements
             if (uri != null) {
                 UserPreference.setToneSettings(getActivity(), uri.toString());
                 tone = uri.toString();
+
+                setRingToneText(tone);
             } else {
                 UserPreference.setToneSettings(getActivity(), RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION).toString());
                 tone = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION).toString();
+
+                setRingToneText(tone);
             }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.notify_tone:
+                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_NOTIFICATION);
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, Uri.parse(tone));
+                this.startActivityForResult(intent, 5);
+                break;
         }
     }
 }
