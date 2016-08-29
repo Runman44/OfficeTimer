bb068043f6a1a4b640759363f6ac16ef6bc9d673
Dennis Anderson <info@mranderson.nl>
2016-08-29 17:50:10 +0200

    refactored the walking service

--- app/src/main/java/nl/mranderson/sittingapp/service/ActivityRecognitionIntentService.java
+++ app/src/main/java/nl/mranderson/sittingapp/service/ActivityRecognitionIntentService.java
@@ -11,9 +11,17 @@ import org.greenrobot.eventbus.EventBus;
 import nl.mranderson.sittingapp.UserPreference;
 import nl.mranderson.sittingapp.events.WalkingEvent;
 
+import static com.google.android.gms.location.DetectedActivity.ON_BICYCLE;
+import static com.google.android.gms.location.DetectedActivity.ON_FOOT;
+import static com.google.android.gms.location.DetectedActivity.RUNNING;
+import static com.google.android.gms.location.DetectedActivity.STILL;
+import static com.google.android.gms.location.DetectedActivity.TILTING;
+import static com.google.android.gms.location.DetectedActivity.WALKING;
+
 public class ActivityRecognitionIntentService extends IntentService {
 
-    public static final int CONFIDENCE_METER_PERCENTAGE = 80;
+    private static final int CONFIDENCE_METER_PERCENTAGE = 70;
+    private static final int CONFIDENCE_METER_CERTAIN_PERCENTAGE = 90;
 
     public ActivityRecognitionIntentService() {
         super("ActivityRecognitionIntentService");
@@ -22,43 +30,17 @@ public class ActivityRecognitionIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         if (ActivityRecognitionResult.hasResult(intent)) {
-            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
-            DetectedActivity detectedActivity = result.getMostProbableActivity();
+            DetectedActivity detectedActivity = ActivityRecognitionResult.extractResult(intent).getMostProbableActivity();
 
             int confidence = detectedActivity.getConfidence();
-            String mostProbableName = getActivityName(detectedActivity.getType());
-            if (mostProbableName.equals("On Foot") || mostProbableName.equals("On Bicycle") && confidence > CONFIDENCE_METER_PERCENTAGE) {
-
+            int type = detectedActivity.getType();
+            if (type == ON_FOOT || type == ON_BICYCLE || type == RUNNING || type == WALKING && confidence > CONFIDENCE_METER_PERCENTAGE) {
                 UserPreference.setUserWalked(this, true);
                 EventBus.getDefault().post(new WalkingEvent(true));
-
-            } else if (mostProbableName.equals("Still") && confidence > CONFIDENCE_METER_PERCENTAGE && UserPreference.getUserWalked(this)) {
-
+            } else if ((type == STILL && confidence > CONFIDENCE_METER_PERCENTAGE) || (type == TILTING && confidence > CONFIDENCE_METER_CERTAIN_PERCENTAGE) && UserPreference.getUserWalked(this)) {
                 UserPreference.setUserWalked(this, false);
                 EventBus.getDefault().post(new WalkingEvent(false));
             }
         }
-    }
-
-    private String getActivityName(int type) {
-        switch (type) {
-            case DetectedActivity.IN_VEHICLE:
-                return "In Vehicle";
-            case DetectedActivity.ON_BICYCLE:
-                return "On Bicycle";
-            case DetectedActivity.ON_FOOT:
-                return "On Foot";
-            case DetectedActivity.WALKING:
-                return "Walking";
-            case DetectedActivity.STILL:
-                return "Still";
-            case DetectedActivity.TILTING:
-                return "Tilting";
-            case DetectedActivity.RUNNING:
-                return "Running";
-            case DetectedActivity.UNKNOWN:
-                return "Unknown";
-        }
-        return "N/A";
     }
 }
