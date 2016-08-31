96f275eefae2e2041ef43cf16dee07b1e7fe20e4
Dennis Anderson <info@mranderson.nl>
2016-08-31 08:45:43 +0200

    version bump, delete unused dependencies, change logging

--- app/src/main/java/nl/mranderson/sittingapp/activity/TimerActivity.java
+++ app/src/main/java/nl/mranderson/sittingapp/activity/TimerActivity.java
@@ -167,6 +167,7 @@ public class TimerActivity extends AppCompatActivity implements GoogleApiClient.
                 onWalkingEvent(new WalkingEvent(true));
                 break;
             case R.id.bStop:
+                Utils.logFirebaseEvent("STOP_TIMER");
                 onBackPressed();
         }
     }
@@ -241,8 +242,6 @@ public class TimerActivity extends AppCompatActivity implements GoogleApiClient.
 
         EventBus.getDefault().unregister(this);
         super.onBackPressed();
-
-        Utils.logFirebaseEvent("STOP_TIMER");
     }
 
     private void killTimer() {
